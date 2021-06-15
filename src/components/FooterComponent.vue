53e273ef5585a33f2e22f7fe6bbb15d7679ea18c
MatiasKala <matiraffi@gmail.com>
2021-06-15 02:20:34 -0300

    Agregados Componentes Header y Footer

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footerComponent">
+    <h1>FOOTER</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'footerComponent',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="headerComponent">
+    <h1>HEADER</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'headerComponent',
+  props: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
