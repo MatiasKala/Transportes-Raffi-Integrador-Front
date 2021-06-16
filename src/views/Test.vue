ab8137a5b64fcb8f1c7071e0cce19493c2098aac
MatiasKala <matiraffi@gmail.com>
2021-06-16 02:31:25 -0300

    Creada vista para los Test

---
+++ src/views/Test.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="Test">
+    <Tabla :datos="getUsuarios"/>
+    <button @click="getUsuarios">AAAAAA</button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Tabla from "@/components/Tabla.vue"
+
+export default {
+  name: 'TestView',
+  components: {
+    Tabla
+  },
+  methods:{
+    getUsuarios(){
+      this.axios.get('https://stormy-mountain-51560.herokuapp.com/usuarios').then((response) => {
+        console.log(response.data)
+      })
+    }
+  }
+}
+</script>
