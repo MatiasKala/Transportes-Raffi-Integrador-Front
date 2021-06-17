e589fede731c4a07a9faea892e7738410a48bbea
MatiasKala <matiraffi@gmail.com>
2021-06-17 04:00:15 -0300

    Eliminada dependencia de iconos fa-fa

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,6 +1,5 @@
 <template>
   <b-container fluid class="footerComponent">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <b-row>
 
       <b-col id="seccion">
