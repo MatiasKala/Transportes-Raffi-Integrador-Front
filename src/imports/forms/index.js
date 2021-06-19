04c8f65011a2f6393f287b406dfeab8b295c26da
MatiasKala <matiraffi@gmail.com>
2021-06-19 00:48:12 -0300

    Incorporado nuevo validador para contraseña

--- src/imports/forms/index.js
+++ src/imports/forms/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueForm from 'vue-form'
+import $store from '../store/index'
 
 var options = {
     validators:{
@@ -8,9 +9,11 @@ var options = {
         },
         'no-espacios' : function(value) {
             return !value.includes(' ')
+        },
+        'no-caracteres' : function(value) {
+            return !Array.from(value).some(r=> Array.from($store.state.caracteresNoPermitidosContrasenia).includes(r))
         }
     }
-    
 }
 
 Vue.use(VueForm, options)
