f83fbe13759494c91ed46b4e10bdfa271193a1eb
MatiasKala <matiraffi@gmail.com>
2021-07-03 05:19:32 -0300

    Cambiado back a Heroku en vez de local

--- src/imports/store/index.js
+++ src/imports/store/index.js
@@ -7,8 +7,8 @@ export default new Vuex.Store({
   state: {
     caracteresNoPermitidosContrasenia:'(),$\'%#"=-+*~[]`^{};:<>|°¬?¡!',
  
-    // apiDominio:'https://stormy-mountain-51560.herokuapp.com',
-    apiDominio:'http://localhost:3000',
+    apiDominio:'https://stormy-mountain-51560.herokuapp.com',
+    // apiDominio:'http://localhost:3000',
     
     camposOrdenables:'email,username,apellido,patente,tipo,tipoCobro,CUIT',
     // ACA EMPIEZAN INPUTS
